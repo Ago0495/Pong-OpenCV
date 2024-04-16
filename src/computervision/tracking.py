2d1ca3dbd97aa7a004fb5943a447be904953d781
Surxe <119145352+Surxe@users.noreply.github.com>
2024-04-16 10:49:33 -0400

    Cleaner formatting and frame blurring
    Frames are now blurred before HoughCircle transformed, tracking is now much much more consistent.
    
    Variable naming conventions more standard.
    
    upper_bound and lower_bound used instead of canvas_height and ball_radius.
    
    All vars defined outside of functions are now at the top.
    
    Moved hough logic to a function so that it wasn't repeated for each frame needlessly.
    
    verify_distance() removed as it wasn't necessary.
    
    Rewrote many comments

--- src/computervision/tracking.py
+++ src/computervision/tracking.py
@@ -21,6 +21,15 @@ paddle_width = 8
 global ball_radius
 ball_radius = 10
 
+# Right boundary of the canvas
+right_bound = canvas_width-paddle_offset-paddle_width
+# Upper boundary of the canvas
+upper_bound = canvas_height - ball_radius
+# Lower boundary of the canvas
+lower_bound = 0+ball_radius
+
+trajectories = [] #Store past trajectories to calculate the average until the next bounce
+
 def capture_game():
     with mss.mss() as sct:
         # Set the region to capture (coordinates and size of the game window)
@@ -62,11 +71,8 @@ def capture_game():
             else:
                 frame_tracked = img_previous
 
-            # Draw a dot in each of the 4 corners for reference
-            cv2.circle(frame_tracked, (0, 0), 5, color, -1)
-            cv2.circle(frame_tracked, (0, game_region['height']), 5, color, -1)
-            cv2.circle(frame_tracked, (game_region['width'], 0), 5, color, -1)
-            cv2.circle(frame_tracked, (game_region['width'], game_region['height']), 5, color, -1)
+            
+            
                 
             cv2.imshow('Game Capture', frame_tracked)
 
@@ -83,32 +89,47 @@ def capture_game():
 
         cv2.destroyAllWindows()
 
-# Function to track ball movement
-trajectories = [] #Store past trajectories to calculate the average until the next bounce
-def track_ball_movement(frame, prev_frame):
-    # # Convert frames to grayscale for processing
-    gray1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+# Given a frame, detect and return circles using HoughCircles
+def hough_circles(frame):
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    prev_circle = cv2.HoughCircles(gray2, cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=5,maxRadius=20)
-    curr_circle = cv2.HoughCircles(gray1, cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=5,maxRadius=20)
+    # Gaussian blur 5x5
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Detect circles using HoughCircles
+    circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=5, maxRadius=20)
+
+    return circles
+
+# Track the movement of the ball
+def track_ball_movement(curr_frame, prev_curr_frame):
+    # Use HoughCircles to detect the ball in the current and previous frame
+    prev_circle = hough_circles(prev_curr_frame)
+    curr_circle = hough_circles(curr_frame)
     
+    # If no circles are detected in either frame, return the current frame without a circle drawn
     if prev_circle is None or curr_circle is None:
-        return frame
+        return curr_frame
     
     # Grab just the first circle found from each
     prev_circle = prev_circle[:1]
     curr_circle = curr_circle[:1]
 
+    # Convert the circles to integer values
     curr_circle = np.uint16(np.around(curr_circle))
 
     # If ball is moving right, predict point to show where the ball will land
     if curr_circle[0][0][0] > prev_circle[0][0][0]:
-        if verify_distance(prev_circle[0][0], curr_circle[0][0]):
+
+        # Clear the trajectories if the ball is past the right paddle
+        if prev_circle[0][0][0] > canvas_width-paddle_offset-paddle_width:
+            trajectories.clear()
+        else:
             # Calculate slope of the ball's trajectory
             slope = (curr_circle[0][0][1] - prev_circle[0][0][1]) / (curr_circle[0][0][0] - prev_circle[0][0][0])
             
-            # Track the average trajectory of every frame before the last bounce
+            # Track the average trajectory of every curr_frame before the last bounce
             trajectory = (curr_circle[0][0][0], curr_circle[0][0][1], slope)
 
             # If the new trajectory has a slope of opposite direction to the previous trajectory, clear the trajectories
@@ -126,75 +147,36 @@ def track_ball_movement(frame, prev_frame):
             new_x, new_y, slope = predict_trajectory(trajectory[0], trajectory[1], trajectory[2])
 
             # Draw the circle around the ball
-            cv2.circle(frame,(curr_circle[0][0][0],curr_circle[0][0][1]),10,(0,255,0),2)
+            cv2.circle(curr_frame,(curr_circle[0][0][0],curr_circle[0][0][1]),10,(0,255,0),2)
 
             # Draw the predicted point
             if new_x is not None:
-                cv2.circle(frame, (int(new_x), int(new_y)), 5, (0, 0, 255), 2)
-
-    return frame
-
-distances = [] #List to store distances between consecutive points, used to skip outliers
-global distances_size_cap
-
-# Function to verify the distance between two points
-# If it is either past the paddle or the distance is an outlier, clear trajectories and don't predict the point
-distances_size_cap = 7 #maximum length of distances list to store
-def verify_distance(point1, point2):
-    x1, y1, _ = point1
-    x2, y2, _ = point2
-
-    # Skip if the ball is already past the paddle
-    if x2>canvas_width-paddle_offset-paddle_width:
-        trajectories.clear() # Clear trajectories if the ball is past the paddle
-        return False
-
-    # Calculate distance between point1 and point2
-    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-
-    # Skip outliers by keeping track of the last 5 distances
-    # Outliers are when the ball "teleports" due to reaching game bounds and restarting in the middle
-    if len(distances) >= distances_size_cap:
-        distances.pop(0)
-
-    # # If the distance is an outlier
-    # if len(distances) > distances_size_cap and distance > 1.5 * max(distances):
-    #     # clear trajectories to prevent the average from being skewed
-    #     trajectories.clear()
-    #     return False
-    
-    
-    distances.append(distance)
-    return True
+                cv2.circle(curr_frame, (int(new_x), int(new_y)), 5, (0, 0, 255), 2)
+    return curr_frame
 
 # Function to predict the linear trajectory of the ball
 # If the ball's slope points it to the bottom or top of the screen, invert the slope and set new point to the border, call self recursively
 # Until arriving at the right side of the screen, return the point
-right_bound = canvas_width-paddle_offset-paddle_width
-canvas_height -= ball_radius
 def predict_trajectory(x, y, slope):
     # If F(width) is within 0 and the canvas height, return the final boundary point
     new_x = right_bound
     new_y = slope * (new_x-x) + y
-    if new_y > ball_radius and new_y < canvas_height:
+    if new_y > lower_bound and new_y < upper_bound:
         return new_x, new_y, slope
 
-    # If F(width) is above the canvas height or below 0,
-    # invert the slope and set the new point to the boundary
-    elif new_y > canvas_height:
-        new_y = canvas_height
-        #bounces
-        return predict_trajectory((new_y - y) / slope + x, new_y, -slope)  # Pass clear_trajectories=True
-    
-    # If F(width) is below 0
-    elif new_y < ball_radius:
-        new_y = ball_radius
-        #bounces
-        return predict_trajectory((new_y - y) / slope + x, new_y, -slope)  # Pass clear_trajectories=True
+    # If F(width) is: 
 
-    # ball is not moving right
-    trajectories.clear() # Clear trajectories if the ball is not moving right
-    return None, None, None  
+    # Above the upper bound - Invert the slope, set new point to F_inverse(upper_bound) and call recursively
+    elif new_y > upper_bound:
+        new_y = upper_bound
+        return predict_trajectory((new_y - y) / slope + x, new_y, -slope)  
+    # Below below lower bound - Invert the slope, set new point to F_inverse(lower_bound) and call recursively
+    elif new_y < lower_bound:
+        new_y = lower_bound
+        return predict_trajectory((new_y - y) / slope + x, new_y, -slope)  
+
+    # ball is not moving right but was missed by initial check
+    raise ValueError("predict_trajectory(): Ball is not moving right but was missed by the check in track_ball_movement()") 
 
 
 if __name__ == "__main__":
